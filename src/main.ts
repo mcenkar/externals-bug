b0916e302c82426ffa8fbcaccb6d6ca52dd25116
Maciej Cenkar <maciej.cenkar@leonteq.com>
2023-10-19 10:33:26 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import 'v-calendar';
+
+// import 'v-calendar/dist/style.css';
+import ('v-calendar/dist/style.css');
+
+createApp(App).mount('#app')
