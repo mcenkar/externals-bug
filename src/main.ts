55ca0b9391f253d610bffeeb239f109980813800
Maciej Cenkar <maciej.cenkar@leonteq.com>
2023-10-20 08:14:58 +0200

    ?used hack

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import App from './App.vue'
 import 'v-calendar';
 
 // import 'v-calendar/dist/style.css';
-import ('v-calendar/dist/style.css');
+// @ts-ignore
+import ('v-calendar/dist/style.css?used');
 
 createApp(App).mount('#app')
